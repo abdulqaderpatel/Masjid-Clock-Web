369e4a954c2fd848ae7c78770c46a8e626a7e7ac
abdulqaderpatel <abdulqaderpatel2002@gmail.com>
2024-07-22 21:45:24 +0530

    improved signup page ui significantly

---
+++ src/components/AuthUserTypeCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps({
+  isSelected: Boolean,
+  selectedIcon: String,
+  unSelectedIcon: String,
+  title: String,
+});
+
+defineEmits(["onCardClick"]);
+</script>
+
+<template>
+  <div
+    class="w-[200px] h-[150px] border-2 border-gray-300 flex flex-col gap-2 justify-center items-center"
+    :class="{ 'border-blue-500': isSelected }"
+    @click="$emit('onCardClick')"
+  >
+    <template v-if="isSelected">
+      <img :src="selectedIcon" width="75" alt="" />
+    </template>
+    <template v-else>
+      <img :src="unSelectedIcon" width="75" alt="" />
+    </template>
+    <p class="font-medium" :class="{ 'text-blue-500': isSelected }">
+      {{ title }}
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/authentication/SignupView.vue
+++ src/views/authentication/SignupView.vue
@@ -14,6 +14,11 @@ import VerifyEmail from "./VerifyEmail.vue";
 import { jwtDecode } from "jwt-decode";
 import type Masjid from "@/models/Masjid";
 import { useMasjidStore } from "@/stores/masjidStore";
+import AuthUserTypeCard from "@/components/AuthUserTypeCard.vue";
+import MasjidFilledIcon from "../../assets/mosque_icon_filled.png";
+import MasjidPlainIcon from "../../assets/mosque_icon_plain.png";
+import UserFilledIcon from "../../assets/user_icon_filled.png";
+import UserPlainIcon from "../../assets/user_icon_plain.png";
 
 let masjidStore = useMasjidStore();
 
@@ -21,11 +26,15 @@ const formData = ref({
   name: "",
   email: "",
   password: "",
-  country: "US",
-  state: "AK",
+  country: "",
+  state: "",
   city: "",
   address: "",
 });
+
+const userType = ref("user");
+
+console.log(userType.value);
 
 const isButtonLoading = ref(false);
 const showModal = ref(false);
@@ -102,89 +111,117 @@ async function signup() {
 </script>
 
 <template>
-  <div class="mx-auto p-4 max-w-90 flex flex-col items-center gap-2">
-    <h1 class="font-bold text-4xl self-center">Welcome to Masjid Clock</h1>
-    <p class="text-gray-800 text-base mb-4">
-      Upload your masjid time table and we will handle it Lorem ipsum dolor sit
-      amet consectetur adipisicing elit. Illum, harum.
-    </p>
-    <form
-      class="md:container-xl grid grid-cols-1 md:grid-cols-2 gap-4 max-w-[800px]"
-      @submit.prevent="signup"
-    >
-      <div class="col-span-2 md:col-span-1">
-        <AuthInputBox
-          title="Name"
-          v-model="formData.name"
-          placeholder="Enter Name"
+  <div class="mx-auto p-4 w-max flex flex-col items-center gap-2">
+    <h1 class="font-bold text-4xl self-center">Register</h1>
+    <p class="text-gray-800 text-base mb-4">Manage your masjid schedule</p>
+    <div class="flex w-max justify-between items-center gap-10">
+      <div class="hidden md:w-[400px] lg:w[500px] bg-blue-500 lg:flex flex-col">
+        <div class="p-4">
+          <h1 class="font-bold text-4xl text-center text-white mb-3">
+            Masjid Clock
+          </h1>
+          <p class="text-gray-300 mb-3 text-2xl font-semibold text-center">
+            Your masjid Schedule, our responsibility
+          </p>
+        </div>
+        <img
+          src="../../assets/masjid_register.jpg"
+          width="400px"
+          alt="masjid image"
         />
       </div>
 
-      <div class="col-span-2 md:col-span-1">
-        <AuthInputBox
-          title="Email"
-          v-model="formData.email"
-          placeholder="Enter Email Address"
-          :input-type="Input.EMAIL"
-        />
-      </div>
-
-      <div class="col-span-2 md:col-span-1">
-        <AuthInputBox
-          title="Password"
-          v-model="formData.password"
-          class="mb-4"
-          placeholder="Enter Password"
-          :input-type="Input.PASSWORD"
-        />
-      </div>
-
-      <div class="col-span-2 md:col-span-1">
-        <AuthLabel title="Country" />
-        <country-select
-          class="shadow bg-white border-gray-400 focus:outline-none focus:border-gray-900 border-2 rounded px-2 py-2 placeholder:text-base block mb-4 w-full"
-          v-model="formData.country"
-          country="US"
-        />
-      </div>
-
-      <div class="col-span-2 md:col-span-1">
-        <AuthLabel title="Region" />
-        <region-select
-          class="shadow bg-white border-gray-400 focus:outline-none focus:border-gray-900 border-2 rounded px-2 py-2 placeholder:text-base block mb-4 w-full"
-          v-model="formData.state"
-          :country="formData.country"
-          :region="formData.state"
-        />
-      </div>
-
-      <div class="col-span-2 md:col-span-1">
-        <AuthInputBox
-          title="City"
-          class="mb-4"
-          v-model="formData.city"
-          placeholder="Enter City"
-        />
-      </div>
-
-      <div
-        class="col-span-2 md:row-span-2 flex flex-col justify-center align-center"
+      <form
+        class="sm:max-w-[500px] grid grid-cols-1 md:grid-cols-2 gap-4"
+        @submit.prevent="signup"
       >
-        <AuthLabel title="Address" />
-        <textarea
-          type="text"
-          v-model="formData.address"
-          placeholder="Enter Address"
-          rows="4"
-          class="shadow border-gray-400 focus:outline-none focus:border-gray-900 border-2 rounded px-2 py-2 placeholder:text-base block mb-8 w-1/2"
+        <AuthUserTypeCard
+          :is-selected="userType == 'user'"
+          :selected-icon="UserFilledIcon"
+          :un-selected-icon="UserPlainIcon"
+          title="User"
+          @click="userType = 'user'"
         />
-      </div>
+        <AuthUserTypeCard
+          :is-selected="userType == 'mosque'"
+          :selected-icon="MasjidFilledIcon"
+          :un-selected-icon="MasjidPlainIcon"
+          title="Masjid"
+          @click="userType = 'mosque'"
+        />
 
-      <div class="col-span-2 flex justify-center">
-        <AppButton title="Register" :isLoading="isButtonLoading" />
-      </div>
-    </form>
+        <div class="col-span-2 md:col-span-1">
+          <AuthInputBox
+            title="Name"
+            v-model="formData.name"
+            placeholder="Enter Name"
+          />
+        </div>
 
+        <div class="col-span-2 md:col-span-1">
+          <AuthInputBox
+            title="Email"
+            v-model="formData.email"
+            placeholder="Enter Email Address"
+            :input-type="Input.EMAIL"
+          />
+        </div>
+
+        <div class="col-span-2 md:col-span-1">
+          <AuthInputBox
+            title="Password"
+            v-model="formData.password"
+            class="mb-4"
+            placeholder="Enter Password"
+            :input-type="Input.PASSWORD"
+          />
+        </div>
+
+        <div class="col-span-2 md:col-span-1">
+          <AuthLabel title="Country" />
+          <country-select
+            placeholder="no"
+            class="shadow bg-white border-gray-100 focus:outline-none focus:border-gray-900 border-2 rounded px-2 py-2 placeholder:text-base block mb-4 w-full"
+            v-model="formData.country"
+            country="US"
+          />
+        </div>
+
+        <div class="col-span-2 md:col-span-1">
+          <AuthLabel title="Region" />
+          <region-select
+            class="shadow bg-white border-gray-100 focus:outline-none focus:border-gray-900 border-2 rounded px-2 py-2 placeholder:text-base block mb-4 w-full"
+            v-model="formData.state"
+            :country="formData.country"
+            :region="formData.state"
+          />
+        </div>
+
+        <div class="col-span-2 md:col-span-1">
+          <AuthInputBox
+            title="City"
+            class="mb-4"
+            v-model="formData.city"
+            placeholder="Enter City"
+          />
+        </div>
+
+        <div class="col-span-2 flex flex-col justify-center align-center">
+          <AuthLabel title="Address" />
+          <textarea
+            type="text"
+            v-model="formData.address"
+            placeholder="Enter Address"
+            rows="4"
+            class="shadow border-gray-100 focus:outline-none focus:border-gray-900 border-2 rounded px-2 py-2 placeholder:text-base block mb-8 w-full"
+          />
+        </div>
+
+        <div class="col-span-2 flex justify-center">
+          <AppButton title="Register" :isLoading="isButtonLoading" />
+        </div>
+      </form>
+    </div>
     <Modal
       v-if="showModal"
       :show="showModal"
